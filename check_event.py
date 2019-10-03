b0446162da80d78e269e22af7390369ca4b897d9
ZJUHSY <690757034@qq.com>
2019-10-03 22:33:32 +0800

    'annotate'

--- check_event.py
+++ check_event.py
@@ -23,8 +23,17 @@ import datetime
 import matplotlib as mpl
 from scipy.special import comb
 
-global all_time_arr
-def find_events(win_len, gran, band_width, event_back_len):
+
+# select all the event point index
+# inout: win_len: windows length for smoothing and choosing events range
+# inputL gran: granuliarity
+# input: band_width: the parameters set to choose detect event points: larger event point will be smaller
+# input: event_back_len: the event backtrading length:
+# output: all_time_arr: the overall array of time used for another function only
+# output: event_time_set: list (set of event time)
+
+def find_events(win_len, gran, band_width, event_back_len):  # select all the event point index
+
     inp = open('test.json', 'rb')
     data = json.load(inp)
     data = pd.DataFrame(data)
@@ -66,6 +75,7 @@ def find_events(win_len, gran, band_width, event_back_len):
     datetime_arr = [datetime.datetime.strptime(x, myFormat) for x in datetime_arr]  # change to datetime obj
     all_time_arr = pd.to_datetime(datetime_arr)
 
+
     time_index = []  # init
     if gran == 'day':
         time_index = [x.replace(hour=0, minute=0) for x in
@@ -103,18 +113,19 @@ def find_events(win_len, gran, band_width, event_back_len):
     power_std = tmp['power'].rolling(window=event_back_len, min_periods=2).std().shift()
     up_band = np.array(power_avg + band_width * power_std)
     event_time_lst = np.where((tmp['power'].values > up_band) == True)[0].tolist()  # extract all event time index
-    event_idx_lst = []  # get each ecent's index: list of list
+    #event_idx_lst = []  # get each ecent's index: list of list
     event_time_set = []  # get all event time used to label
 
     for event_time in event_time_lst:
         event_time_set.append(tmp.index[event_time])
         # sel_idx = find_index(str(tmp.index[event_time]), gran, all_time_arr, win_len)
         # event_idx_lst.append(sel_idx)
-    return event_time_set
+    return event_time_set, all_time_arr
 
     # plot
 
 
+#plot smooth_scale
 def plot_smooth_semantic(tmp, gran):  # input groupby data to plot smooth semantics
     _xlabel = 'time' + 'granularity: ' + gran
     _path = 'semantic_plot--' + args.start_time + '--' + args.end_time + '--' + gran + '.jpg'
@@ -127,19 +138,24 @@ def plot_smooth_semantic(tmp, gran):  # input groupby data to plot smooth semant
     plt.savefig(_path)
 
 
-def find_index(sel_time, gran, all_time_arr, win_len):
+###get index from one event
+# input: sel_time type:str the event time
+# input: gran: granuliarity
+# input: windows length
+# output: selected index for the current event
+def find_index(sel_time, gran, win_len):
     if gran == 'day':
-        _from = datetime.datetime.strptime(sel_time, '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d')
+        _from = datetime.datetime.strptime(str(sel_time), '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d')
         _from = datetime.datetime.strptime(_from, '%Y-%m-%d')
         _from = _from + datetime.timedelta(days=-(win_len // 2))
         _to = _from + datetime.timedelta(days=win_len)
     elif gran == 'hour':
-        _from = datetime.datetime.strptime(sel_time, '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d %H')
+        _from = datetime.datetime.strptime(str(sel_time), '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d %H')
         _from = datetime.datetime.strptime(_from, '%Y-%m-%d %H')
         _from = _from + datetime.timedelta(hours=-(win_len // 2))
         _to = _from + datetime.timedelta(hours=win_len)
     elif gran == 'min':
-        _from = datetime.datetime.strptime(sel_time, '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d %H:%M')
+        _from = datetime.datetime.strptime(str(sel_time), '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d %H:%M')
         _from = datetime.datetime.strptime(_from, '%Y-%m-%d %H:%M')
         _from = _from + datetime.timedelta(minutes=-(win_len // 2))
         _to = _from + datetime.timedelta(minutes=win_len)
@@ -148,6 +164,7 @@ def find_index(sel_time, gran, all_time_arr, win_len):
     return sel_idx
 
 
+#main program used to test funtion
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-st_time", '--start_time', type=str, default=None)
@@ -161,9 +178,12 @@ if __name__ == '__main__':
     parser.add_argument('-ebl', '--event_back_len', type=int, default = 15)
     args = parser.parse_args()
 
-    event_time_set = find_events(args.win_len, args.granularity, args.band_width, args.event_back_len)
+    event_time_set, all_time_arr = find_events(args.win_len, args.granularity, args.band_width, args.event_back_len)
+
+    if len(event_time_set) == 0:
+        print('No event detcted! You can change the paramerers to loose the event detection criteria!')
     all_idx = []
     for event_time in event_time_set:
-        sel_idx = find_index(str(event_time), args.gran, all_time_arr, args.win_len)  # use to extract one index
+        sel_idx = find_index(event_time, args.gran, args.win_len)  # use to extract one index
         # event_idx_lst.append(sel_idx)
         all_idx.append(sel_idx)
